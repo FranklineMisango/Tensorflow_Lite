fa9f1532fd8b229e4f6479b3bbb6de5a4046f2dc
misango <franklinemisango@gmail.com>
2023-07-14 12:54:50 +0800

    Final UI/UX

--- DL_classifier/app/src/main/java/com/example/dl_classifier/MainActivity.java
+++ DL_classifier/app/src/main/java/com/example/dl_classifier/MainActivity.java
@@ -2,13 +2,34 @@ package com.example.dl_classifier;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
+import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button selectBtn, predictBtn, captureBtn;
+    TextView result;
+    ImageView imageview;
+    Bitmap bitmap;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        selectBtn = findViewById(R.id.selectbtn);
+        predictBtn = findViewById(R.id.predictbtn);
+        captureBtn = findViewById(R.id.capturebtn);
+        result = findViewById(R.id.result);
+        imageview = findViewById(R.id.imageview);
+
+
+
     }
 }
