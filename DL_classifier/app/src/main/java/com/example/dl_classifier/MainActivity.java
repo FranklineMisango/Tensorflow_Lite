724a7add6f140e115299c9429c4879467db4d4bf
misango <franklinemisango@gmail.com>
2023-07-14 13:10:33 +0800

    Image addition finished

--- DL_classifier/app/src/main/java/com/example/dl_classifier/MainActivity.java
+++ DL_classifier/app/src/main/java/com/example/dl_classifier/MainActivity.java
@@ -1,14 +1,20 @@
 package com.example.dl_classifier;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.media.Image;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -29,6 +35,35 @@ public class MainActivity extends AppCompatActivity {
         result = findViewById(R.id.result);
         imageview = findViewById(R.id.imageview);
 
+        selectBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                intent.setType("Image/*");
+                startActivityForResult(intent, 10);
+
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode,int resultCode, @Nullable Intent data){
+        if (resultCode==10) {
+            if (data != null){
+                Uri uri = data.getData();
+                try{
+                    bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), uri);
+                    imageview.setImageBitmap(bitmap);
+                }
+                catch (IOException e){
+                    e.printStackTrace();
+                }
+
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data);
 
 
     }
